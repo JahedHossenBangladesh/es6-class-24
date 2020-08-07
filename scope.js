ea007ffaf25fa6906be983e6bf99665c4b7e2171
JahedHossenBangladesh <jahedhossen4358.com>
2020-08-07 20:43:31 +0600

    this is scope

---
+++ scope.js
@@ -0,0 +1,19 @@
+let bonus = 20;
+
+
+function sum(first,second){
+    let result = first + second + bonus;
+    console.log( result);
+    if(result > 9){
+        // const mood = "happy";
+        var mood = "happy";
+        console.log(mood)
+    }
+    console.log(mood);
+    console.log(day);
+    
+    return result;
+}
+const output = sum(3,7);
+
+console.log( output);
