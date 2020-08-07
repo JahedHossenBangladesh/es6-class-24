01d44a1e4bbea3581c56e9a03b7845b0cf0d1be6
JahedHossenBangladesh <jahedhossen4358.com>
2020-08-07 15:44:31 +0600

    Null Vs undifine is clear for Interview

---
+++ nullVsUndifine.js
@@ -0,0 +1,20 @@
+//let pakhi;
+
+// function add(num1,num2){
+//     console.log( num1 + num2);
+//     return 
+// }
+// function add( num1,num2){
+//     console.log( num1 , num2);
+// }
+// const result = add( 10);
+// const premik = {
+//     name:"Smart Boy",
+//     phone: 24455
+// }
+let fun= undefined;
+console.log(fun);
+let ages= [23,44,44];
+console.log(ages[11]);
+
+
