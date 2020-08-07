aa10a10cd3f6b2f1b32b7dd9ba4fa028ab92fbf8
JahedHossenBangladesh <jahedhossen4358.com>
2020-08-07 15:15:02 +0600

    this is js

---
+++ trueFalse.js
@@ -0,0 +1,28 @@
+// the false
+//0
+//"" empty string
+// null
+// undefine
+
+
+
+//const age = -4; //that is also true
+//const age = 0; that is false 
+//const age = 4; that is true
+//const name = "solaiman" that is true
+//const name = "";  // thai is false for "" but true for " ";
+// const name = null ; // thai is aso false
+let name = NaN;
+
+
+// let name;
+// console.log(name); that is undifine and also false
+
+
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
